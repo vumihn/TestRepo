4b0a8fbb5993998ac12b1d5382ab3e7b8bc60f11
Minh <minhnguyen21@augustana.edu>
2023-09-28 00:32:48 -0500

    "Now with dancing chicken."

--- SillyBirdProject/src/main/java/edu/augustana/Main.java
+++ SillyBirdProject/src/main/java/edu/augustana/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello bird world!");
         Chicken myChicken = new Chicken();
         myChicken.dance();
+        System.out.println("test");
     }
 }
